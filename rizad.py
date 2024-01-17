07607b9a4dd7e97848e491fbdadcf4e8543ac8c2
Lioneruser4 <117773578+Lioneruser4@users.noreply.github.com>
2024-01-17 17:52:04 +0400

    Update rizad.py

--- rizad.py
+++ rizad.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 import os
 
 BOT_TOKEN = "2138035413:AAFUuaAoCSdauCOyo8eB0kgCsFHbai9IfEY"
-    API_ID = "9501538"
+    API_ID = "9501538",
     API_HASH = "adb8864f52095ff4ca53e847a9250dac"
 
     if __name__ == "__main__" :
